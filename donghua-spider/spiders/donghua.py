8a6d8ba3e5b1bbb186d00491ee923dbc82418587
zhufree <zhufree2013@gmail.com>
2022-11-26 17:03:21 +0800

    :sparkles: next page

--- donghua-spider/spiders/donghua.py
+++ donghua-spider/spiders/donghua.py
@@ -4,8 +4,10 @@ import scrapy
 class DonghuaSpider(scrapy.Spider):
     name = 'donghua'
     allowed_domains = ['api.bilibili.com']
+    page_index = 1
     start_urls = ['https://api.bilibili.com/pgc/season/index/result?order=3&st=4&sort=0&page=1&season_type=4&pagesize=20&type=1']
-
+    base_url = 'https://api.bilibili.com/pgc/season/index/result?order=3&st=4&sort=0&page={}&season_type=4&pagesize=20&type=1'
+    
     def parse(self, response):
         res_json = response.json()
         data_list = res_json['data']['list']
@@ -17,3 +19,8 @@ class DonghuaSpider(scrapy.Spider):
                 'score': i['score']
             }
             yield donghua
+        self.page_index += 1
+        if self.page_index <= 51:
+            next_page_url = self.base_url.format(self.page_index)
+            print(next_page_url)
+            yield scrapy.Request(next_page_url, callback=self.parse)
